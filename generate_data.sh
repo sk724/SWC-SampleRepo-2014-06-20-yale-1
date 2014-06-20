39f709e00eb5b65fcbeb764c8539a8a653add8f3
sabrinacales <bri.cales@gmail.com>
2014-06-20 13:59:19 -0400

    added a comment to generate_data.sm

--- generate_data.sh
+++ generate_data.sh
@@ -1,7 +1,7 @@
 # Use a for loop to create a text file called data1.txt with lines 1 through
 # 10 (HINT: You can append to the end of a file with >>)
 
-# A new comment
+# this is fun
 
 for i in {1..10} ;
 do
