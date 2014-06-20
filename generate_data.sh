5a4fa00bd1fe5dba203abb18c8bd6419c301c97a
sabrinacales <bri.cales@gmail.com>
2014-06-20 11:26:30 -0400

    Added a new comment

--- generate_data.sh
+++ generate_data.sh
@@ -1,6 +1,8 @@
 # Use a for loop to create a text file called data1.txt with lines 1 through
 # 10 (HINT: You can append to the end of a file with >>)
 
+# A new comment
+
 for i in {1..10} ;
 do
 	echo $i >> "data1.txt"
